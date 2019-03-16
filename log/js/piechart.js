beb1b4616681cef03078bf523d8d4106bd1fb6d2
alexkurland <akurland@brightplan.com>
2019-03-15 18:32:02 -0700

    pretty colors in graphs

--- log/js/piechart.js
+++ log/js/piechart.js
@@ -1,21 +1,28 @@
-function get(){
+function get() {
 
 }
-let osArray;
-let osAmounts;
+let osColors = [];
+let osBorders = [];
+let osArray = [];
+let osAmounts = [];
+let browserColors = [];
+let browserBorders = [];
+let browserArray = [];
+let browserAmounts = [];
 $.ajax({
   type: "GET",
   url: "/charts/pie/os",
   //data: data
   success: function(data) {
-    console.log(data)
-    console.log("HI");
     data.map(os => {
-      console.log(os.os);
-      osArray = os.os;
-      osAmounts = os.count;
-      console.log(os.count);
+      osArray.push(os.os);
+      osAmounts.push(os.count);
+      osColors.push(colorSorter(os.os));
+      osBorders.push("#FFFFFF");
+      console.log(osArray);
+      console.log(osAmounts);
     })
+
   },
   headers: {
     "Content-type": "application/json"
@@ -23,98 +30,97 @@ $.ajax({
   xhrFields: {
     withCredentials: true
   },
-});
+}).then(test => {
+  $(function() {
 
+    //get the pie chart canvas
+    var ctx1 = $("#pie-chartcanvas-1");
+    var ctx2 = $("#pie-chartcanvas-2");
 
-$(function(){
-
-  //get the pie chart canvas
-  var ctx1 = $("#pie-chartcanvas-1");
-  var ctx2 = $("#pie-chartcanvas-2");
-
-  //pie chart data
-  var data1 = {
-    labels: ["match1", "match2", "match3", "match4", "match5"],
-    datasets: [
-      {
+    //pie chart data
+    var data1 = {
+      labels: osArray,
+      datasets: [{
         label: "TeamA Score",
-        data: [10, 50, 25, 70, 40],
-        backgroundColor: [
-          "#DEB887",
-          "#A9A9A9",
-          "#DC143C",
-          "#F4A460",
-          "#2E8B57"
-        ],
-        borderColor: [
-          "#CDA776",
-          "#989898",
-          "#CB252B",
-          "#E39371",
-          "#1D7A46"
-        ],
-        borderWidth: [1, 1, 1, 1, 1]
-      }
-    ]
-  };
+        data: osAmounts,
+        backgroundColor: osColors,
+        borderColor: osBorders//,
+        //borderWidth: [1, 1, 1, 1, 1]
+      }]
+    };
 
-  //pie chart data
-  var data2 = {
-    labels: ["match1", "match2", "match3", "match4", "match5"],
-    datasets: [
-      {
-        label: "TeamB Score",
-        data: [20, 35, 40, 60, 50],
-        backgroundColor: [
-          "#FAEBD7",
-          "#DCDCDC",
-          "#E9967A",
-          "#F5DEB3",
-          "#9ACD32"
-        ],
-        borderColor: [
-          "#E9DAC6",
-          "#CBCBCB",
-          "#D88569",
-          "#E4CDA2",
-          "#89BC21"
-        ],
-        borderWidth: [1, 1, 1, 1, 1]
-      }
-    ]
-  };
+    //pie chart data
+    var data2 = {
+      labels: browserArray,
+      datasets: [{
+        label: "TeamA Score",
+        data: browserAmounts,
+        backgroundColor: browserColors,
+        borderColor: browserBorders
+      }]
+    };
 
-  //options
-  var options = {
-    responsive: true,
-    title: {
-      display: true,
-      position: "top",
-      text: "Pie Chart",
-      fontSize: 18,
-      fontColor: "#111"
-    },
-    legend: {
-      display: true,
-      position: "bottom",
-      labels: {
-        fontColor: "#333",
-        fontSize: 16
+    //options
+    var options = {
+      responsive: true,
+      title: {
+        display: true,
+        position: "top",
+        text: "Pie Chart",
+        fontSize: 18,
+        fontColor: "#111"
+      },
+      legend: {
+        display: true,
+        position: "bottom",
+        labels: {
+          fontColor: "#333",
+          fontSize: 16
+        }
       }
-    }
-  };
+    };
 
-  //create Chart class object
-  var chart1 = new Chart(ctx1, {
-    type: "pie",
-    data: data1,
-    options: options
+    //create Chart class object
+    var chart1 = new Chart(ctx1, {
+      type: "pie",
+      data: data1,
+      options: options
+    });
+
+    //create Chart class object
+    var chart2 = new Chart(ctx2, {
+      type: "pie",
+      data: data2,
+      options: options
+    });
   });
+})
 
-  //create Chart class object
-  var chart2 = new Chart(ctx2, {
-    type: "pie",
-    data: data2,
-    options: options
-  });
-});
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function colorSorter(o) {
+  var ret;
+  if (o.includes("Windows")) {
+    ret = "#a2c8ec";
+  }
+  else if (o.includes("Mac")) {
+    ret = "#ceb6d9";
+  }
+  else if (o.includes("Linux")) {
+    ret = "#e3abce";
+  }
+  else if (o.includes("Ubuntu")){
+    ret = "#fbcabb";
+  }
+  else{
+    ret = getRandomColor;
+  }
+  return ret;
+}
